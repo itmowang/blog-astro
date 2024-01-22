6cd3d4c94faeb96e4b9a09e7d91b0091b29036f1
Mowang <mw@loli.wang>
2024-01-22 10:23:33 +0800

    ✨ feat: 增加统计

---
+++ docs/_astro/hoisted.c0ed0f4d.js
@@ -0,0 +1,1 @@
+window.dataLayer=window.dataLayer||[];function e(){dataLayer.push(arguments)}e("js",new Date);e("config","G-RH0BZRF88N");const t=document.querySelector(".menu-btn"),n=document.querySelector(".sidebar");t?.addEventListener("click",()=>{n?.classList.toggle("full-height")});
--- docs/_astro/hoisted.eeacada9.js
+++
@@ -1,1 +0,0 @@
-const e=document.querySelector(".menu-btn"),t=document.querySelector(".sidebar");e?.addEventListener("click",()=>{t?.classList.toggle("full-height")});
